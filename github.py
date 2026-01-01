d04a76547356fb61e2ae1cdcb3f8eacb06595193
Rajvi Dave <rajvidave22@gmail.com>
2026-01-01 12:33:25 +0530

    file saving

--- github.py
+++ github.py
@@ -4,4 +4,9 @@ import nltk
 response=requests.get("https://github.com/ukhirani")
 print(response.status_code)
 content=(response.content)
-words=nltk.corpus.content.words()
+
+file_path = "new_file.txt"
+with open(file_path, 'w') as file:
+    file.write(content)
+
+print({file_path})
