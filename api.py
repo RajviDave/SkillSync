1d7e78ffb11c577c3c258f286eeccab54b77e930
RajviDave <rajvidave22@gmail.com>
2026-01-09 10:54:51 +0530

    openai api done

--- api.py
+++ api.py
@@ -1,13 +1,9 @@
-dict={"name":"rajvi","number":4,"surname":"dave","city":"rajkot"}
-dic1={}
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
 
-for dic in dict:
-    
-    if dic in dic1:
-        print("noting")
-    else:
-        dic1[dic]=dict[dic]
+load_dotenv()
 
-print(dict)
-print(dic1)
+client=OpenAI()
 
+print("Client initialized successfully!")
