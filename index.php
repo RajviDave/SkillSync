582a22a87a5399f0818c69f306171f5d228d6fef
RajviDave <rajvidave22@gmail.com>
2026-01-30 20:29:34 +0530

    signup done

--- index.php
+++ index.php
@@ -8,34 +8,44 @@
 </head>
 <body> 
 
+    <form action="index.php" method="post">
+    <input type="text" name="username" value="rajvidave"><br>
+    <input type="text" name="email" value="Email"><br>
+    <input type="password" name="passwd" value="Passwd"><br>
+    <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+
 <?php
-    $username=$email=$passwd="";
-    $usernamERR=$emailERR=$passwdERR="";
-    $usernamecharERR=$emailcharERR="";
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $username=$_POST["username"];
+    $email=$_POST["email"];
+    $passwd=$_POST["passwd"];
+    $usernameERR=$emailERR=$passwdERR=" ";
+    $usernamecharERR=$emailcharERR=$passcharERR=" ";
+    
 
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         if (empty($_POST["username"])){
             $usernameERR="username is requirement";
-        }elseif(!preg_match(`/^[A-Za-z0-9]+$/`,$username)){
+        }elseif(!preg_match('/^[A-Za-z0-9]+$/',$username)){
             $usernamecharERR="Only characters and numbers are allowed";
         }
 
         if(empty($_POST["email"])){
             $emailERR="Email is required";
-        }elseif(!preg_match(`@`,$email)){
+        }elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
             $emailcharERR="Enter correct email address";
         }
 
-        if(empty($_POST["password"])){
-            $passWD="Enter password";
-        }elseif((strlen($_POST["password"]))<8){
-            echo "Enter 8 character long password";
+        if(empty($_POST["passwd"])){
+            $passwdERR="Enter password";
+        }elseif((strlen($_POST["passwd"]))<8){
+            $passcharERR="Enter 8 character long password";
         }
             
         }
+
+        echo $username;
+}
 ?>
-    <input type="text" name="email" value="<?php echo "$email";?>"><br>
-    <input type="password" name="passwd" value="<?php echo "$passwd";?>"><br>
-    <input type="text" name="username" value="<?php echo "$username";?>"><br>
-    <input type="submit" name="submit" value="SUBMIT">
-</body>
