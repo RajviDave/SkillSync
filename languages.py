ab52353872a19860b1fb74da5e89cade5bcf56d4
RajviDave <rajvidave22@gmail.com>
2026-02-02 19:30:12 +0530

    languages

---
+++ languages.py
@@ -0,0 +1,22 @@
+domain_to_languages = {
+
+"embedded_systems": ["C","C++"],
+"vlsi": ["Verilog","SystemVerilog","TCL"],
+"frontend_web": ["JavaScript","TypeScript"],
+"backend_web": ["Python","Java","Node.js"],
+"fullstack_web": ["JavaScript","TypeScript","Python"],
+"mobile_app": ["Kotlin","Swift","Dart"],
+"ml_ai_dl_cv": ["Python"],
+"data_science": ["Python","R"],
+"data_engineering": ["Python","SQL","Scala"],
+"data_analytics": ["SQL","Python"],
+"cloud": ["Python","Go"],
+"devops": ["Python","Go","Bash"],
+"cyber_security": ["Python","C"],
+"ar_vr_xr": ["C#","C++"],
+"game_development": ["C#","C++"],
+"iot": ["C","Python"],
+"robotics": ["Python","C++"],
+"blockchain": ["Solidity","JavaScript"],
+
+}
