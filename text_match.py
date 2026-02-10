a6a44705232eb8621a0c54ec80fc5df273b00fc3
RajviDave <rajvidave22@gmail.com>
2026-02-10 21:59:24 +0530

    jd processing done

--- text_match.py
+++ text_match.py
@@ -17,11 +17,15 @@ tokens = word_tokenize(j_d.lower())
 filtered_tokens = [word for word in tokens if word not in stop_words]
 # print(filtered_tokens)
 
+matched_domains={}
 for domain,keyword_list in domain_keywords.items():
     for keyword in keyword_list:
         if " " in keyword:
             if keyword in clean_text:
-                print(domain)
+                matched_domains[domain] = matched_domains.get(domain, 0) + 1  
         else:
             if keyword in filtered_tokens:
-                print(domain)
+                matched_domains[domain] = matched_domains.get(domain, 0) + 1
+            
+
+print(matched_domains)
