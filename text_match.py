13b2a7ca4fb828e1e7d22f3f017e6871139139a3
RajviDave <rajvidave22@gmail.com>
2026-02-10 20:40:31 +0530

    domain-keyword logic

--- text_match.py
+++ text_match.py
@@ -1,8 +1,24 @@
 from domain import *
 from resume import domain_skills
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-jd=input("Enter job description = ")
-jd=jd.lower()
+j_d=input("Enter job description = ")
+jd=j_d.lower()
 jd=re.sub(r'[^a-zA-Z0-9\s]', ' ', jd)
-print(jd)
+clean_text=jd
+# print(clean_text)
+
+stop_words = set(stopwords.words('english'))
+tokens = word_tokenize(j_d.lower())
+
+filtered_tokens = [word for word in tokens if word not in stop_words]
+# print(filtered_tokens)
+
+for domain,keyword_list in domain_keywords.items():
+    for keyword in keyword_list:
+        if " " in keyword:
+            if keyword in filtered_tokens:
+                print(keyword)
